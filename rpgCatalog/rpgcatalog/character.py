8a0aba7af4e51a1ada03d508f0ff1771bb4afcd3
Olivier PIETREMONT <olivier.pietremont@wescale.fr>
2023-01-17 13:30:28 +0100

    Iteration 1 step 1.2

--- rpgCatalog/rpgcatalog/character.py
+++ rpgCatalog/rpgcatalog/character.py
@@ -1,7 +1,9 @@
 class Character:
 
-    def __init__(self, health: int = 1000):
+    def __init__(self, health: int = 1000, level: int = 1, alive: bool = True):
         """
-        Character begin with 1000 health point.
+        Character begin with 1000 health point and level 1.
         """
         self.health = health
+        self.level = level
+        self.alive = alive
--- rpgCatalog/tests/test_rpg.py
+++ rpgCatalog/tests/test_rpg.py
@@ -5,3 +5,15 @@ def test_character_begin_with_1000_point_health():
     steve = Character()
 
     assert steve.health == 1000
+
+
+def test_character_begin_with_level_1():
+    steve = Character()
+
+    assert steve.level == 1
+
+
+def test_character_begin_alive():
+    steve = Character()
+
+    assert steve.alive
